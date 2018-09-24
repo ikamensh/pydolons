65e9dd7f50a95f1da1733568c5fcdf6c2c895d99
Mukhutdinov Reef <reef425@gmail.com>
2018-09-25 00:07:08 +0500

    add class Cell tests

---
+++ tests/battlefield/test_cell.py
@@ -0,0 +1,19 @@
+from battlefield import Cell
+
+class Foo:
+    def __init__(self):
+        pass
+
+def test_cell():
+    a = Cell(1, 2)
+    b = Cell(2, 3)
+    c = Cell(1, 2)
+    f = Foo()
+    f.x = 1
+    f.y = 2
+
+    assert (a == None) == False
+    assert (a == f) == False
+    assert (a == b) == False
+    assert (a == c) == True
+    assert (a == a) == True
