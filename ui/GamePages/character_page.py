5823179631ac22e44875a35af2cfc34a38598073
Mukhutdinov Reef <reef425@gmail.com>
2018-09-27 03:12:26 +0500

    raname folder

---
+++ ui/GamePages/character_page.py
@@ -0,0 +1,23 @@
+class CharacterPage(object):
+    """docstring for CharacterPage."""
+    def __init__(self):
+        super(CharacterPage, self).__init__()
+        # self.arg = arg
+
+    def setUp(self, arg):
+        """
+        Устанавливаем настройки страницы
+        """
+        pass
+
+    def showPage(self, arg):
+        """
+        Показать  страницу
+        """
+        pass
+
+    def updatePage(self, arg):
+        """
+        Обновить страницу
+        """
+        pass
