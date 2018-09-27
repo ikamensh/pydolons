97619aabcc4d2db99cfa81d70acbb8c0768ad05a
Mukhutdinov Reef <reef425@gmail.com>
2018-09-27 08:48:17 +0500

    refactoring:   def moveUnit add:   def setLevel   def diedUnit

--- ui/units/Units.py
+++ ui/units/Units.py
@@ -5,51 +5,40 @@ class Units(QtWidgets.QGraphicsItemGroup):
     def __init__(self, *arg):
         super(Units, self).__init__(*arg)
         self.active_unit = None
-        self.units_location = {}
         self.units_at = {}
-        self.units_bf = {}
         self.units_stack = None
+        self.level = None
 
+    def setLevel(self, level):
+        self.level =  level
+        self.level.units = self
 
     def getUitsLocations(self):
         for uid, unit in self.units_at.items():
             if uid != self.active_unit.uid:
                 yield unit.getWorldPos()
 
-    def getUnit(self, x, y):
-        return self.units_location.setdefault((x, y), None)
+    def moveUnit(self, unit, cell_to):
+        x, y = cell_to.x, cell_to.y
+        self.units_at[unit.uid].setWorldPos(x, y)
 
-
-    def updateLocations(self, unit, pos):
-        del self.units_location[pos]
-        self.units_location[(unit.worldPos.x, unit.worldPos.y)] = unit
-
+    def dieadUnit(self, unit):
+        unit = self.units_at[uid]
+        self.removeFromGroup(unit)
+        del self.units.units_at[unit.uid]
 
     def collisionHeorOfUnits(self, x = None, y = None):
         if x is None:
             if not (self.active_unit.worldPos.x, y) in self.getUitsLocations():
-                pos = (self.active_unit.worldPos.x, self.active_unit.worldPos.y)
                 self.active_unit.setWorldY(y)
-                self.updateLocations(self.active_unit, pos)
+                # self.moveUnit(self.active_unit, self.active_unit.worldPos)
         elif y is None:
             if not (x, self.active_unit.worldPos.y) in self.getUitsLocations():
-                pos = (self.active_unit.worldPos.x, self.active_unit.worldPos.y)
                 self.active_unit.setWorldX(x)
-                self.updateLocations(self.active_unit, pos)
-
-
-    def removeUnit(self, uid):
-        unit = self.units_at[uid]
-        self.removeFromGroup(unit)
-        del self.units_location[(unit.worldPos.x, unit.worldPos.y)]
-        self.locations.remove(unit.worldPos)
-        del self.units_bf[uid]
-        del self.units_at[uid]
-
+                # self.moveUnit(self.active_unit, self.active_unit.worldPos)
 
     def setActiveUnit(self, unit):
         self.active_unit = self.units_at[unit.uid]
-
 
     def setUnitStack(self, units):
         self.units_stack = units
