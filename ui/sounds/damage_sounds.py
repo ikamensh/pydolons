b66cfa2c33dfe214fcc13b19bb7acc04cca9216a
Mukhutdinov Reef <reef425@gmail.com>
2018-09-28 00:58:38 +0500

    bad sound files

--- ui/sounds/damage_sounds.py
+++ ui/sounds/damage_sounds.py
@@ -1,18 +1,25 @@
 from mechanics.damage import DamageTypes
+import os
 
-prefix = "resources/sounds/damage/"
+sep = os.path.sep
+prefix = 'resources' + sep + 'sounds' + sep + 'damage'
+prefix = os.path.abspath(prefix)
+
+print(prefix)
 
 class sound_paths:
-    slash = prefix + "slash.wav"
-    bash = prefix + "bash.wav"
-    pierce = prefix + "pierce.wav"
+    slash = os.path.join(prefix, 'slash.wav')
+    bash = os.path.join(prefix, 'bash.wav')
 
-    fire = prefix + "fire.wav"
-    ice = prefix + "ice.wav"
-    lightning = prefix + "lightning.wav"
-    acid = prefix + "acid.wav"
+    # pierce = prefix + "pierce.wav"
+    pierce = os.path.join(prefix, 'pierce.wav')
 
-    sonic = prefix + "sonic.wav"
+    fire = os.path.join(prefix, 'fire.wav')
+    ice = os.path.join(prefix, 'ice.wav')
+    lightning = os.path.join(prefix, 'lightning.wav')
+    acid = os.path.join(prefix, 'acid.wav')
+
+    sonic = os.path.join(prefix, 'sonic.wav')
 
 
 damage_sounds = {}
@@ -20,20 +27,17 @@ t = DamageTypes
 
 from ui.sounds.sound import Sound
 
+
 damage_sounds[t.SLASH] = sound_paths.slash
 damage_sounds[t.CRUSH] = sound_paths.bash
-damage_sounds[t.PIERCE] = sound_paths.pierce
+# damage_sounds[t.PIERCE] = sound_paths.pierce
 
-damage_sounds[t.FIRE] = sound_paths.fire
-damage_sounds[t.FROST] = sound_paths.ice
-damage_sounds[t.LIGHTNING] = sound_paths.lightning
-damage_sounds[t.ACID] = sound_paths.acid
+# damage_sounds[t.FIRE] = sound_paths.fire
+# damage_sounds[t.FROST] = sound_paths.ice
+# damage_sounds[t.LIGHTNING] = sound_paths.lightning
+# damage_sounds[t.ACID] = sound_paths.acid
 
-damage_sounds[t.SONIC] = sound_paths.sonic
+# damage_sounds[t.SONIC] = sound_paths.sonic
 
 for k, v in damage_sounds.items():
     damage_sounds[k] = Sound(v)
-
-
-
-
--- ui/sounds/sound.py
+++ ui/sounds/sound.py
@@ -1,9 +1,13 @@
 import pygame
 
-pygame.mixer.init()
+# pygame.mixer.init()
+# pygame.init()
+
+pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
+pygame.init() #turn all of pygame on.
+
 Sound = pygame.mixer.Sound
 
 # from PySide2.QtMultimedia import QSound as Sound
 
 # import vlc
-
