54f464c7c4dc1474bbbac1f6ed85df880f3006bc
Mukhutdinov Reef <reef425@gmail.com>
2018-09-27 08:50:45 +0500

    add new base class BaseLevel add slot for signal GameProxyChanel
    add slot unitMoveSlot, unitDieadSlot, unitDamageSlot

--- ui/levels.py
+++ ui/levels.py
@@ -4,7 +4,7 @@ from PySide2 import QtCore
 from ui.gameworld import GameWorld, MidleLayer
 from content.dungeons.demo_dungeon import demo_dungeon
 from ui.units import Units, BasicUnit
-
+from ui.gui_util.GameProxyChanel import GameProxyChanel
 
 class DemoGameTread(QtCore.QThread):
 
@@ -23,42 +23,72 @@ class DemoGameTread(QtCore.QThread):
     def run(self):
         self.finished.emit(self.createDemoDungeon())
 
+class BaseLevel(object):
+    def __init__(self):
+        super(BaseLevel, self).__init__()
+        self.gameRoot = None
+        self.middleLayer = None
+        self.world = None
+        self.units = None
 
+    def setGameRoot(self, gameRoot):
+        self.gameRoot =  gameRoot
+        self.gameRoot.currentLevel = self
 
-class Level_demo_dungeon:
+    def setMiddleLayer(self, middleLayer):
+        self.middleLayer = middleLayer
+        self.middleLayer.level =  self
+
+    def setGameWorld(self, world):
+        self.world = world
+        self.world.level = self
+
+    def setUnits(self, units):
+        self.units = units
+        self.units.level = self
+
+class Level_demo_dungeon(BaseLevel):
 
     def __init__(self, gameconfig):
+        super(Level_demo_dungeon, self).__init__()
         self.gameconfig = gameconfig
         self.z_values = [ i for i in range(demo_dungeon.w)]
+        self.gamechanel = GameProxyChanel()
+        self.gamechanel.unitDied.connect(self.unitDiedSlot)
+        self.gamechanel.unitMove.connect(self.unitMoveSlot)
+        self.gamechanel.targetDamage.connect(self.targetDamageSlot)
 
-    def unitDied(self, unit):
-        del self.units.units_at[unit]
-        p = self.units.unit_locations[unit].x, self.units.unit_locations[unit].y
-        del self.units.locations[p]
-        del self.units.unit_locations[unit]
+    def unitDiedSlot(self, msg):
+        print('died slot run')
+        self.units.dieadUnit(msg.get('unit'))
+        self.middleLayer.removeUnitLayer(msg.get('unit').uid)
 
+    def unitMoveSlot(self, msg):
+        print('move slot run')
+        self.units.moveUnit(msg.get('unit'), msg.get('cell_to'))
+        self.middleLayer.moveSupport(self.units.units_at[msg.get('unit').uid])
+
+    def targetDamageSlot(self, msg):
+        print('target Damage run')
+        self.middleLayer.updateSupport(msg.get('target'), msg.get('amount'))
 
     def setUpLevel(self, game, controller):
-        self.world = GameWorld(self.gameconfig)
+        self.setGameWorld(GameWorld(self.gameconfig))
         self.world.setWorldSize(demo_dungeon.w, demo_dungeon.h)
         self.world.setFloor(self.gameconfig.getPicFile('floor.png'))
 
-        self.middleLayer = MidleLayer(self.gameconfig)
-        self.middleLayer.setUpLevel(self)
+        self.setMiddleLayer(MidleLayer(self.gameconfig))
         self.game = game
 
         self.setUpUnits(self.game.battlefield)
         self.gameconfig.setWorld(self.world)
         controller.setUp(self.world, self.units, self.middleLayer)
 
-
     def printResult(self, time):
         print('time = ',time)
 
     def setUpUnits(self, battlefield):
-        self.units = Units()
-        self.units.unit_locations = {}
-        self.units.locations = []
+        self.setUnits(Units())
 
         for unit, unit_pos in battlefield.unit_locations.items():
             gameUnit = BasicUnit(self.gameconfig.unit_size[0], self.gameconfig.unit_size[1], gameconfig = self.gameconfig)
@@ -66,15 +96,11 @@ class Level_demo_dungeon:
                 self.active_unit = True
             gameUnit.setPixmap(self.gameconfig.getPicFile(unit.icon))
             gameUnit.setWorldPos(unit_pos.x, unit_pos.y)
-            print('uid = ',unit.uid)
             gameUnit.uid = unit.uid
-
             self.units.addToGroup(gameUnit)
-            self.units.units_bf[unit.uid] = unit
+            # добавили gameunit
             self.units.units_at[unit.uid] = gameUnit
-            self.units.units_location[(gameUnit.worldPos.x, gameUnit.worldPos.y)] = gameUnit
-            self.units.locations.append(gameUnit.worldPos)
 
         self.units.active_unit = self.units.units_at[self.game.turns_manager.get_next().uid]
         self.units.setUnitStack(self.game.turns_manager.managed)
-        self.middleLayer.createHPBar()
+        self.middleLayer.createSuppot(self.units.units_at)
