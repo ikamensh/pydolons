96827bc91c4ef3605f4296ad316757663e669b9b
Mukhutdinov Reef <reef425@gmail.com>
2018-09-28 00:00:50 +0500

    add creation GameMenu

--- ui/GamePages/__init__.py
+++ ui/GamePages/__init__.py
@@ -4,11 +4,23 @@
 Страницы обновляются по мере необходимости.
 """
 
+from ui.GamePages.GameMenuPage import ScreenMenu
+
 class GamePages(object):
     """docstring for GamePages."""
     def __init__(self):
         super(GamePages, self).__init__()
-    self.gameRoot = None
+        self.gameRoot = None
 
     def setGameRoot(self, gameRoot):
         self.gameRoot =  gameRoot
+
+    def setUpGameMenu(self):
+        self.gameMenu = ScreenMenu()
+        self.scene.addItem(self.gameMenu)
+        self.gameMenu.setUpGui()
+        self.gameMenu.setUpConsole()
+
+
+    def updateGui(self):
+        self.gameMenu.updateGui()
