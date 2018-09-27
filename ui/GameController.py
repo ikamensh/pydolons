2095778731b8cecc59c0257eb94e839bc251ea60
Mukhutdinov Reef <reef425@gmail.com>
2018-09-28 01:16:31 +0500

    renma file

--- ui/GameController.py
+++ ui/GameController.py
@@ -3,22 +3,24 @@ from PySide2 import QtCore, QtGui, QtWidgets
 
 
 class GameController:
-    def __init__(self, gameconfig, game, view, scene, menu):
+    def __init__(self,  game):
         """
         last_point = Cell() -- последняя точка, на которую указывает игрок
         """
-        self.gameconfig = gameconfig
         self.the_game = game
-        self.view = view
-        self.scene = scene
-        self.screenMenu = menu
 
+        self.gameRoot = None
         self.tr = QtGui.QTransform()
 
         self.cursor = QtWidgets.QGraphicsEllipseItem(-10, -10, 20, 20)
 
         self.last_point = Cell(0, 0)
         self.selected_point = Cell(0, 0)
+
+
+    def setGameRoot(self, gameRoot):
+        self.gameRoot =  gameRoot
+        self.gameRoot.cfg = self
 
 
     def setUp(self, world, units, middleLayer):
@@ -33,7 +35,7 @@ class GameController:
     def mouseMoveEvent(self, e):
         """ Метод перехватывает событие движение мыши
         """
-        newPos = self.view.mapToScene(e.x(), e.y())
+        newPos = self.view.mapTogameRoot.scene(e.x(), e.y())
         self.moveCursor(newPos)
         self.itemSelect(newPos)
 
@@ -68,44 +70,44 @@ class GameController:
 
     def moveScene(self, rect, x, y):
         rect.translate(x, y)
-        self.scene.setSceneRect(rect)
-        self.screenMenu.setDefaultPos()
+        self.gameRoot.scene.setSceneRect(rect)
+        self.gameRoot.gameMenu.setDefaultPos()
 
-    def translateScene(self, e):
+    def translatScene(self, e):
         """Данный метод обеспечивает перемещение сцены внутри представления
          метод проверяет приблизился ли курсор к краю представления
         """
-        rect = self.scene.sceneRect()
+        rect = self.gameRoot.scene.sceneRect()
         if e.x() - 5.0 < 5.0:
             rect.translate(-10, 0)
-            self.scene.setSceneRect(rect)
-            self.screenMenu.setDefaultPos()
+            self.gameRoot.scene.setSceneRect(rect)
+            self.gameRoot.gameMenu.setDefaultPos()
 
         if e.x() + 5.0 > self.view.viewport().width() - 5.0:
             rect.translate(10, 0)
-            self.scene.setSceneRect(rect)
-            self.screenMenu.setDefaultPos()
+            self.gameRoot.scene.setSceneRect(rect)
+            self.gameRoot.gameMenu.setDefaultPos()
 
         if e.y() - 5.0 < 5.0:
             rect.translate(0, -10)
-            self.scene.setSceneRect(rect)
-            self.screenMenu.setDefaultPos()
+            self.gameRoot.scene.setSceneRect(rect)
+            self.gameRoot.gameMenu.setDefaultPos()
 
         if e.y() + 5.0 > self.view.viewport().height() - 5.0:
             rect.translate(0, 10)
-            self.scene.setSceneRect(rect)
-            self.screenMenu.setDefaultPos()
+            self.gameRoot.scene.setSceneRect(rect)
+            self.gameRoot.gameMenu.setDefaultPos()
 
     def itemSelect(self, newPos):
-        x = int((newPos.x() / self.tr.m11()) / self.gameconfig.unit_size[0])
+        x = int((newPos.x() / self.tr.m11()) / self.gameRoot.cfg.unit_size[0])
         if newPos.x() < 0:
             x -= 1
 
-        y = int((newPos.y() / self.tr.m11()) / self.gameconfig.unit_size[1])
+        y = int((newPos.y() / self.tr.m11()) / self.gameRoot.cfg.unit_size[1])
         if newPos.y() < 0:
              y -= 1
 
-        world_x, world_y = self.gameconfig.world_size
+        world_x, world_y = self.gameRoot.cfg.world_size
         if 0 <= x < world_x and  0 <= y < world_y:
             self.last_point.x, self.last_point.y = x, y
 
