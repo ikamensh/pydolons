ff0982fcb79c7e6cdd204cdbccaa6603e27e11ac
Mukhutdinov Reef <reef425@gmail.com>
2018-09-29 13:40:35 +0500

    commented out unused code

--- ui/sounds/sound.py
+++ ui/sounds/sound.py
@@ -1,12 +1,21 @@
-import pygame
+# import pygame
 
 # pygame.mixer.init()
 # pygame.init()
 
-pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
-pygame.init() #turn all of pygame on.
+# pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
+# pygame.init() #turn all of pygame on.
 
-Sound = pygame.mixer.Sound
+# Sound = pygame.mixer.Sound
+
+class Sound(object):
+    """docstring for Sound."""
+    def __init__(self, arg):
+        super(Sound, self).__init__()
+        self.arg = arg
+
+
+# Sound = object
 
 # from PySide2.QtMultimedia import QSound as Sound
 
