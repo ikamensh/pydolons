6312e313fff32236a9d62ca7cb60092ea18cb6bf
Mukhutdinov Reef <reef425@gmail.com>
2018-09-29 16:56:11 +0500

    add new import

--- ui/units/__init__.py
+++ ui/units/__init__.py
@@ -1,2 +1,5 @@
 from ui.units.BasicUnit import BasicUnit
 from ui.units.Units import Units
+from ui.units.HealthText import HealthText
+from ui.units.HealthBar import HealthBar
+from ui.units.UnitMiddleLayer import UnitMiddleLayer
