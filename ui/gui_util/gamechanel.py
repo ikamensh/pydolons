ad1a8c426a594467aa4ca71a770a9757733f0294
Mukhutdinov Reef <reef425@gmail.com>
2018-09-29 13:39:04 +0500

    add stack system for chanel

--- ui/gui_util/gamechanel.py
+++ ui/gui_util/gamechanel.py
@@ -4,11 +4,15 @@ class GameChanel(object):
     def __init__(self):
         super(GameChanel, self).__init__()
         self.message = {}
+        self.msg_stack = []
 
     def sendMessage(self, message):
         self.message = message
+        self.msg_stack.append(message)
 
     def getMessage(self):
+        # print(self.msg_stack)
+        # print('->'.join([msg.get('event') for msg in self.msg_stack ]))
         message = self.message
         self.message = {}
         return message
