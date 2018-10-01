0782f5205c44005c84cdfb5ad4b3f73f18cfcafb
Mukhutdinov Reef <reef425@gmail.com>
2018-10-01 21:23:00 +0500

    add send msg to gamechanel

--- mechanics/events/ActiveEvent.py
+++ mechanics/events/ActiveEvent.py
@@ -1,6 +1,7 @@
 from mechanics.events.src.Event import Event
 from mechanics.events import EventsChannels
 import my_context
+from ui.gui_util.gamechanel import gamechanel
 
 class ActiveEvent(Event):
     channel = EventsChannels.ActiveChannel
@@ -18,6 +19,7 @@ class ActiveEvent(Event):
         # print(type(self.active))
         # print('self.targeting dir :\n',type(self.targeting))
         self.active.resolve(self.targeting)
+        gamechanel.sendMessage({'event':'ActiveEvent','target':self.targeting, 'active':self.active})
 
     def __repr__(self):
         return f"{self.active.owner} activates {self.active}, {self.targeting}"
