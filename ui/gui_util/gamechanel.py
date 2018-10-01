bc70bc6629cf3adf817169a6adcf58a601bd74cf
Mukhutdinov Reef <reef425@gmail.com>
2018-10-01 21:20:13 +0500

    remove not used code

--- ui/gui_util/gamechanel.py
+++ ui/gui_util/gamechanel.py
@@ -1,3 +1,4 @@
+import my_context
 
 class GameChanel(object):
     """docstring for GameChanel."""
@@ -7,14 +8,15 @@ class GameChanel(object):
         self.msg_stack = []
 
     def sendMessage(self, message):
-        self.message = message
-        self.msg_stack.append(message)
+        # self.message = message
+        if not my_context.the_game.is_sim:
+            self.msg_stack.append(message)
 
-    def getMessage(self):
-        # print(self.msg_stack)
-        # print('->'.join([msg.get('event') for msg in self.msg_stack ]))
-        message = self.message
-        self.message = {}
-        return message
+    # def getMessage(self):
+    #     # print(self.msg_stack)
+    #     # print('->'.join([msg.get('event') for msg in self.msg_stack ]))
+    #     message = self.message
+    #     self.message = {}
+    #     return message
 
 gamechanel = GameChanel()
