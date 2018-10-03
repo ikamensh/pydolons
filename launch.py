b006e1db9b8a4a5d7a399642b54130fb76121a9b
Mukhutdinov Reef <reef425@gmail.com>
2018-10-03 16:49:02 +0500

    add comment

--- launch.py
+++ launch.py
@@ -1,10 +1,13 @@
+# import game_loop object and ProxyEmit
 from GameLoopThread import GameLoopThread, ProxyEmit
+
 from mechanics.AI.SimGame import SimGame as DreamGame
 from content.base_types.demo_hero import demohero_basetype
 from content.dungeons.demo_dungeon import demo_dungeon
 from game_objects.battlefield_objects import Unit
 from ui.TheUI import TheUI
 from threading import Thread
+# import triggers
 from ui.triggers.animation_triggers import move_anim_trigger, damage_anim_trigger, attack_anin_trigger, perish_anim_trigger, turn_anim_trigger
 
 import time
@@ -20,11 +23,13 @@ def one_game():
     DO NOT MODIFY THIS FILE!
 
     """
+    # Qt application initialization
     app = QtWidgets.QApplication(sys.argv)
+    # Logical engine initialization
     game = DreamGame.start_dungeon(demo_dungeon, Unit(demohero_basetype))
+    # Ui engine initialization
     window = TheUI(game)
     TheUI.singleton = window
-
     # NEW TRIGGER ADD THIS |
     #                      V
     turn_anim_trigger(),
@@ -32,18 +37,23 @@ def one_game():
     attack_anin_trigger(),
     damage_anim_trigger(),
     move_anim_trigger()
-
+    # debug print
     game.print_all_units()
-
+    # game_loop thread initialization
     loop = GameLoopThread()
+    # set game and ui engine
     loop.game = game
     loop.the_ui = window
+    # Qt signal initialization
     loop.setSiganls(ProxyEmit)
+    # game_loop run
+    # if the game_loop completes work then thread will completes its work
     loop.start()
-
+    # debug time sleep
     time.sleep(5)
-
+    # debug print
     print(f"battle lasted for {time}")
+    # Qt application exit
     sys.exit(app.exec_())
 
 # t = time()
