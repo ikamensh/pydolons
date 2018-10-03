d23ff992d16781cfa1710b77f8d61e979ea77923
Mukhutdinov Reef <reef425@gmail.com>
2018-10-03 14:11:03 +0500

    refactoring for qt proxy thread worker

--- launch.py
+++ launch.py
@@ -4,14 +4,30 @@ from content.dungeons.demo_dungeon import demo_dungeon
 from game_objects.battlefield_objects import Unit
 from ui.TheUI import TheUI
 from threading import Thread
-from ui.sounds.sound_triggers import attack_sounds_trig, damage_sounds_trig, move_sounds_trig, perish_sounds_trig
+# from ui.sounds.sound_triggers import attack_sounds_trig, damage_sounds_trig, move_sounds_trig, perish_sounds_trig
 from ui.triggers.animation_triggers import move_anim_trigger
 
 import time
 
+import sys
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class WorkerOrder(QtCore.QThread):
+
+    def __init__(self, parent=None):
+        super(WorkerOrder, self).__init__(parent)
+        self.game = None
+
+    def run(self):
+        self.game.loop()
+
 
 def one_game():
+    app = QtWidgets.QApplication(sys.argv)
     game = DreamGame.start_dungeon(demo_dungeon, Unit(demohero_basetype))
+    window = TheUI(game)
+    TheUI.singleton = window
 
 
     #attack_sounds_trig(),
@@ -23,14 +39,20 @@ def one_game():
     print(game)
     game.print_all_units()
 
-    Thread(target=TheUI.launch, args=[game]).start()
+    # Thread(target=TheUI.launch, args=[game]).start()
+    worker = WorkerOrder()
+    worker.game = game
+    worker.start()
+    # Thread(target=game.loop).start()
+    # TheUI.launch(game)
     time.sleep(5)
-    print(TheUI.singleton.gameRoot.cfg)
-    TheUI.singleton.gameRoot.cfg.sound_maps["SftStep3.wav"].play()
+    print('game ready')
+    # print(TheUI.singleton.gameRoot.cfg)
+    # TheUI.singleton.gameRoot.cfg.sound_maps["SftStep3.wav"].play()
+    # game.loop()
 
-
-    game.loop()
     print(f"battle lasted for {time}")
+    sys.exit(app.exec_())
 
 # t = time()
 #
