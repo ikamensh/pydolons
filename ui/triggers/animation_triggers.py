2be34526a4cafc10cbd954f010d1fbc7c776b2ef
Mukhutdinov Reef <reef425@gmail.com>
2018-10-03 14:13:50 +0500

    refactoring and add new trigger damage_anim_trigger and supprot def damage and nit

--- ui/triggers/animation_triggers.py
+++ ui/triggers/animation_triggers.py
@@ -1,19 +1,35 @@
-from mechanics.events import Trigger, MovementEvent
+from mechanics.events import Trigger, DamageEvent, AttackEvent, UnitDiedEvent, MovementEvent
 from ui.triggers.no_sim_condition import no_sim_condition
 from ui.gui_util.gamechanel import gamechanel
 from ui.TheUI import TheUI
+from GameLoopThread import ProxyEmit
 
 ########### DAMAGE #################
 
-def play_movement_anim(t, e):
 
-    TheUI.singleton.gameRoot.cfg.sound_maps["SftStep3.wav"].play()
-    level = TheUI.singleton.gameRoot.level
-    level.units.moveUnit(e.unit, e.cell_to)
-    level.middleLayer.moveSupport(level.units.units_at[e.unit.uid])
+
+def play_movement_anim(t, e):
+    ProxyEmit.play_movement_anim.emit({'unit':e.unit,"cell_to":e.cell_to})
+    pass
 
 
 def move_anim_trigger():
     return Trigger(MovementEvent,
                    conditions={no_sim_condition},
                    callbacks=[play_movement_anim])
+
+
+def maybe_play_damage_anim(t, e):
+    print('debug->damage_trig',dir(e))
+    print('debug->',e.damage.type)
+    ProxyEmit.maybe_play_damage_anim.emit({'type':e})
+    pass
+
+def maybe_play_hit_anim(t, e):
+    print('debug->hit_trig',dir(e))
+    pass
+
+def damage_anim_trigger():
+    return Trigger(DamageEvent,
+                   conditions={no_sim_condition},
+                   callbacks=[maybe_play_damage_anim, maybe_play_hit_anim])
