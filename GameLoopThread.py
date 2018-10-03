b2f7bc458817c9028e9e374b8e06f7677d6de37b
Mukhutdinov Reef <reef425@gmail.com>
2018-10-03 15:53:34 +0500

    add attack trigger

--- GameLoopThread.py
+++ GameLoopThread.py
@@ -12,6 +12,7 @@ class GameLoopThread(QtCore.QThread):
     play_movement_anim = QtCore.Signal(dict)
     maybe_play_damage_anim = QtCore.Signal(dict)
     maybe_play_hit_anim = QtCore.Signal(dict)
+    play_attack_anim = QtCore.Signal(dict)
     def __init__(self, parent=None):
         super(GameLoopThread, self).__init__(parent)
         self.game = None
@@ -21,12 +22,14 @@ class GameLoopThread(QtCore.QThread):
         proxy_cls.play_movement_anim = self.play_movement_anim
         proxy_cls.maybe_play_damage_anim = self.maybe_play_damage_anim
         proxy_cls.maybe_play_hit_anim = self.maybe_play_hit_anim
+        proxy_cls.play_attack_anim = self.play_attack_anim
         self.setConnection()
 
     def setConnection(self):
         self.play_movement_anim.connect(self.the_ui.gameRoot.level.unitMoveSlot)
         self.maybe_play_damage_anim.connect(self.the_ui.gameRoot.level.targetDamageSlot)
         self.maybe_play_hit_anim.connect(self.the_ui.gameRoot.level.targetDamageHitSlot)
+        self.play_attack_anim.connect(self.the_ui.gameRoot.level.attackSlot)
 
     def run(self):
         self.game.loop()
