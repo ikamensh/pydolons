dfd522177485a81d48d98bf0697ad0e741dff3b7
Mukhutdinov Reef <reef425@gmail.com>
2018-10-15 19:45:24 +0500

    refactoring GameMenuPages, add SuWidgetFactory

---
+++ ui/GamePages/suwidgets/SuWidgetFactory.py
@@ -0,0 +1,30 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+
+from ui.GamePages.suwidgets.ToolTip import ToolTip
+from ui.GamePages.suwidgets.NotifyText import NotifyText
+
+from abc import abstractmethod
+
+class SuWidgetFactory:
+    """docstring for SuWidgetFactory."""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def getToolTip(w = 128, h = 128, minimumLeters = 16, fontFamily = "Times", pointSize = 12):
+        tooltip = ToolTip()
+        tooltip.minimumLeters = minimumLeters
+        tooltip.setBrush(QtGui.QBrush(QtCore.Qt.black))
+        tooltip.setOpacity(0.8)
+        tooltip.setDefaultTextColor(QtCore.Qt.white)
+        tooltip.setFont(QtGui.QFont(fontFamily, pointSize, 10, False))
+        tooltip.setTextPos(w / 2, -h / 2)
+        tooltip.setVisible(False)
+        return tooltip
+
+    def getNotifyText(gameRoot):
+        notifytext = NotifyText()
+        notifytext.setFont(QtGui.QFont("Times", 48, 10, False))
+        notifytext.setDefaultTextColor(QtCore.Qt.blue)
+        notifytext.gameRoot = gameRoot
+        return notifytext
