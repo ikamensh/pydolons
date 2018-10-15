e0a877740ffa82dc3e7922e49e4b0ae051b49b9a
Mukhutdinov Reef <reef425@gmail.com>
2018-10-15 19:44:58 +0500

    refactoring GameMenuPages, add ToolTip

---
+++ ui/GamePages/suwidgets/ToolTip.py
@@ -0,0 +1,43 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class ToolTip(QtCore.QObject, QtWidgets.QGraphicsRectItem):
+    """docstring for ToolTip.
+
+    Размер тултипа автоматически подстраивается
+    в методе setText(self, text)
+    self.minimuWidth -- минимальная ширина тултипа, устанвливает в методе setFont()
+    self.minimuWidth Зависит от шрифта и self.minimumLeters -- минимальное количество символов
+
+    """
+    def __init__(self, parent = None):
+        QtCore.QObject.__init__(self, parent)
+        QtWidgets.QGraphicsRectItem.__init__(self, parent)
+        self.textItem = QtWidgets.QGraphicsTextItem()
+        self.textItem.setParentItem(self)
+        self.minimuWidth = 100
+        self.minimumLeters = 16
+
+    def setDefaultTextColor(self, color):
+        self.textItem.setDefaultTextColor(color)
+
+    def setFont(self, font):
+        self.textItem.setFont(font)
+        tempText = self.textItem.toPlainText()
+        self.textItem.setPlainText("#" * self.minimumLeters)
+        self.minimuWidth = self.textItem.boundingRect().width()
+        self.textItem.setPlainText(tempText)
+
+    def setTextPos(self, x, y):
+        self.textItem.setPos(x, y)
+
+    def setText(self, text):
+        self.textItem.setPlainText(text)
+        h = self.textItem.boundingRect().height()
+        self.setRect(self.textItem.x(), self.textItem.y(), self.minimuWidth, h)
+        self.setZValue(10)
+
+    def setDict(self, data):
+        result = ''
+        for k, v in data.items():
+            result = result + k + ' = ' + v + '\n'
+        self.setText(result[:-1])
