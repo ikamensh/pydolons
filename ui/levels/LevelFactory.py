a464eec2d40ad20a53e97d5d2f1f044de53fb09b
reef425 <reef425@gmail.com>
2018-10-26 02:52:52 +0500

    add scene managments, refactoring

--- ui/levels/LevelFactory.py
+++ ui/levels/LevelFactory.py
@@ -1,7 +1,7 @@
 from ui.GameWorld import GameWorld
 from ui.units import UnitMiddleLayer
-from cntent.dungeons.demo_dungeon import demo_dungeon
 from ui.units import Units, BasicUnit
+from game_objects.dungeon.Dungeon import Dungeon
 from game_objects import battlefield_objects as bf_objs
 
 from ui.levels.BaseLevel import BaseLevel
@@ -10,35 +10,44 @@ from ui.levels.BaseLevel import BaseLevel
 class LevelFactory:
     def __init__(self, logicEngine):
         self.LEngine = logicEngine
+        self.gameRoot = None
+        self.dungeon:Dungeon = None
+        self.level:BaseLevel = None
         pass
 
-    def getLevel(self, name):
-        # level = None
-        self.buildLevel(name)
+    def setGameRoot(self, gameRoot):
+        self.gameRoot = gameRoot
+
+    def getLevel(self, levelName = None):
+        self.buildLevel(levelName)
         return self.level
 
-
-    def buildLevel(self, name):
+    def buildLevel(self, levelName = None):
+        if levelName is None:
+            self.dungeon = self.LEngine.dungeon
+        else:
+            self.dungeon = self.LEngine.getDungeon(levelName)
         self.level = BaseLevel()
-        self.z_values = [i for i in range(self.level.w)]
+        self.gameRoot.setLevel(self.level)
+        self.z_values = [i for i in range(self.dungeon.w)]
         self.setUpLevel(self.LEngine.game)
 
 
     def removeLevel(self):
         pass
 
-    def sevaLevelState(self):
+    def saveLevelState(self):
         pass
 
     def setUpLevel(self, game):
         self.level.setGameWorld(GameWorld(self.gameRoot.cfg))
-        self.level.world.setWorldSize(demo_dungeon.w, demo_dungeon.h)
+        self.level.world.setWorldSize(self.dungeon.w, self.dungeon.h)
         self.level.world.setFloor(self.gameRoot.cfg.getPicFile('floor.png'))
 
         self.level.setMiddleLayer(UnitMiddleLayer(self.gameRoot.cfg))
         self.level.game = game
 
-        self.level.setUpUnits(self.level.game.battlefield)
+        self.setUpUnits(self.level.game.battlefield)
         self.level.gameRoot.cfg.setWorld(self.level.world)
         self.level.gameRoot.controller.setUp(self.level.world, self.level.units, self.level.middleLayer)
 
@@ -59,4 +68,16 @@ class LevelFactory:
 
         self.level.units.active_unit = self.level.units.units_at[self.level.game.turns_manager.get_next().uid]
         self.level.middleLayer.createSuppot(self.level.units.units_at)
+
+    def addLevelToScene(self, scene):
+        scene.addItem(self.level.world)
+        scene.addItem(self.level.units)
+        scene.addItem(self.level.middleLayer)
+
+    def removeLevelFromScene(self, scene):
+        scene.removeItem(self.level.world)
+        scene.removeItem(self.level.units)
+        scene.removeItem(self.level.middleLayer)
+
+
 
