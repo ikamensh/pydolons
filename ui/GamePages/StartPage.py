d6a48ed2857047ffaaa562f0985fa00607d70988
reef425 <reef425@gmail.com>
2018-10-30 11:17:36 +0500

    refactoring

--- ui/GamePages/StartPage.py
+++ ui/GamePages/StartPage.py
@@ -7,9 +7,10 @@ class StartPage(AbstractPage):
     def __init__(self, gamePages):
         super().__init__()
         self.gamePages = gamePages
-        self.arg = None
+        self.state = True
         self.w = 240
         self.h = 300
+        self.gamePages.gameRoot.scene.addItem(self)
         self.setUpWidgets()
 
     def setUpWidgets(self):
@@ -17,93 +18,56 @@ class StartPage(AbstractPage):
         self.background.setBrush(QtGui.QBrush(QtCore.Qt.black))
         self.addToGroup(self.background)
 
-        self.mainWidget: QtWidgets.QGraphicsWidget = QtWidgets.QGraphicsWidget(self)
-        self.mainWidget.resize(self.w, self.h)
+        mainWidget: QtWidgets.QWidget = QtWidgets.QWidget()
+        mainWidget.resize(self.w, self.h)
+        mainWidget.setStyleSheet('background-color: rgba(0, 0, 0, 0);')
 
-        self.laoyout :QtWidgets.QGraphicsLinearLayout = QtWidgets.QGraphicsLinearLayout(QtCore.Qt.Vertical)
+        laoyout :QtWidgets.QVBoxLayout = QtWidgets.QVBoxLayout(mainWidget)
         buttonStyle = 'QPushButton{background-color:black;color:white;}QPushButton:pressed{background-color:white;color:black;}'
 
-        self.start = QtWidgets.QPushButton('START')
+        self.start = QtWidgets.QPushButton('START', mainWidget)
         self.start.setStyleSheet(buttonStyle)
-        self.start = self.gamePages.gameRoot.scene.addWidget(self.start)
-        self.laoyout.addItem(self.start)
-        self.start.setParent(self.mainWidget)
+        laoyout.addWidget(self.start)
 
-        self.stop = QtWidgets.QPushButton('STOP')
+        self.stop = QtWidgets.QPushButton('STOP', mainWidget)
         self.stop.setStyleSheet(buttonStyle)
-        self.stop:QtWidgets.QGraphicsProxyWidget = self.gamePages.gameRoot.scene.addWidget(self.stop)
-        self.laoyout.addItem(self.stop)
-        self.stop.setParent(self.mainWidget)
+        self.stop.pressed.connect(self.stopSlot)
+        laoyout.addWidget(self.stop)
 
-        self.settings = QtWidgets.QPushButton('SETTINGS')
-        self.settings.setStyleSheet(buttonStyle)
-        self.settings:QtWidgets.QGraphicsProxyWidget = self.gamePages.gameRoot.scene.addWidget(self.settings)
-        self.laoyout.addItem(self.settings)
-        self.settings.setParent(self.mainWidget)
+        settings = QtWidgets.QPushButton('SETTINGS', mainWidget)
+        settings.setStyleSheet(buttonStyle)
+        laoyout.addWidget(settings)
 
-        self.mainWidget.setLayout(self.laoyout)
-        self.addToGroup(self.mainWidget)
-        self.gamePages.gameRoot.scene.addItem(self)
-        # self.setVisible(False)
+        mainWidget.setLayout(laoyout)
+        self.mainWidget = self.gamePages.gameRoot.scene.addWidget(mainWidget)
         self.resized()
 
     def showPage(self):
         if self.state:
             self.state = False
-            # self.setVisible(False)
             self.gamePages.page = None
             self.gamePages.gameRoot.scene.removeItem(self)
+            self.gamePages.gameRoot.scene.removeItem(self.mainWidget)
         else:
             self.state = True
             self.gamePages.page = self
             self.gamePages.gameRoot.scene.addItem(self)
-            # self.setVisible(True)
-
-    def widgetShape(cls):
-        path = QtGui.QPainterPath()
-        path.addRect(cls.boundingRect())
-        return path
-
-    def widgetMousePressEvent(cls, e):
-        if cls.boundingRect().contains(e.pos()):
-            cls.pressed.emit()
+            self.gamePages.gameRoot.scene.addItem(self.mainWidget)
 
     def resized(self):
         x = (self.gamePages.gameRoot.cfg.dev_size[0] - self.w) / 2
         y = (self.gamePages.gameRoot.cfg.dev_size[1] - self.h) / 2
-        self.mainWidget.moveBy(x, y)
         self.mainWidget.setPos(x, y)
         self.background.setRect(0, 0, self.gamePages.gameRoot.cfg.dev_size[0], self.gamePages.gameRoot.cfg.dev_size[1])
         pass
 
-    def mousePressEvent(self, e):
-        if self.mainWidget.geometry().contains(e.pos()):
-            self.containsWidget(self.start, e)
-            self.containsWidget(self.stop, e)
-            self.containsWidget(self.settings, e)
-
-    def containsWidget(self, widgetProxy, e):
-        rect = widgetProxy.geometry()
-        rect.translate(widgetProxy.parent().pos())
-        if rect.contains(e.pos()):
-            widgetProxy.widget().pressed.emit()
-            widgetProxy.widget().setDown(True)
-
-    def mouseReleaseEvent(self, e):
-        self.mainWidget.mouseReleaseEvent(e)
-        self.start.mouseReleaseEvent(e)
-        self.stop.mouseReleaseEvent(e)
-        self.settings.mousePressEvent(e)
-
     def startSlot(self):
-        self.gamePages.page = self.gamePages.gameMenu
         self.gamePages.page.resized()
-        self.gamePages.gameRoot.scene.removeItem(self)
+        self.showPage()
+        self.gamePages.page = self.gamePages.gameMenu
 
-    # def hide(self):
-    #     self.gamePages.page = self.gamePages.gameMenu
-    #     self.gamePages.page.resized()
-    #     super(StartPage, self).hide()
+    def stopSlot(self):
+        print('stop')
 
 
 
