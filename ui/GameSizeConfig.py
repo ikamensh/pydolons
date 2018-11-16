2d941641b2f4ccd0a99ccd3a6d2dc15b2b96ff86
reef425 <reef45@gmail.com>
2018-11-16 16:05:17 +0500

    add new item MasteriesPage

--- ui/GameSizeConfig.py
+++ ui/GameSizeConfig.py
@@ -4,6 +4,9 @@ def setUpSize():
     # CharacterPage sizes
     gameItemsSizes[101002001] = ((32, 32), (64, 64), (128, 128))
     gameItemsSizes[101002002] = ((128, 128), (256, 256), (384, 384))
+    # MasteriesPage
+    gameItemsSizes[101003001] = ((32, 32), (64, 64), (128, 128))
+    gameItemsSizes[101003002] = ((128, 128), (256, 256), (384, 384))
     return gameItemsSizes
 
 gameItemsSizes = setUpSize()
