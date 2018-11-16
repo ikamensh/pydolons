cbd6a0c4ea114c3c0e602fa671473475e647e63e
reef425 <reef45@gmail.com>
2018-11-16 12:04:57 +0500

    add module GameSizeConfig

---
+++ ui/GameSizeConfig.py
@@ -0,0 +1,9 @@
+
+def setUpSize():
+    gameItemsSizes = {}
+    # CharacterPage sizes
+    gameItemsSizes[101002001] = ((32, 32), (64, 64), (128, 128))
+    gameItemsSizes[101002002] = ((128, 128), (256, 256), (384, 384))
+    return gameItemsSizes
+
+gameItemsSizes = setUpSize()
