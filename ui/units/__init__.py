e7b74c0ee323fac22360712bbeaa104568dba77e
reef425 <reef45@gmail.com>
2018-11-20 19:35:43 +0500

    add new Module GameVision

--- ui/units/__init__.py
+++ ui/units/__init__.py
@@ -3,3 +3,4 @@ from ui.units.Units import Units
 from ui.units.HealthText import HealthText
 from ui.units.HealthBar import HealthBar
 from ui.units.UnitMiddleLayer import UnitMiddleLayer
+from ui.units.GameVision import GameVision
