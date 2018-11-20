4e017fd944b133a401079c50541b3d0a41f55452
reef425 <reef45@gmail.com>
2018-11-20 19:33:29 +0500

    add new items

--- ui/GameSizeConfig.py
+++ ui/GameSizeConfig.py
@@ -10,6 +10,12 @@ def setUpSize():
     # PerksPage
     gameItemsSizes[101004001] = ((32, 32), (64, 64), (128, 128))
     gameItemsSizes[101004002] = ((128, 128), (256, 256), (384, 384))
+    # World size
+    # Floor
+    gameItemsSizes[102001001] = ((128, 128), (256, 256), (256, 256))
+    # GameVision
+    # Shaow
+    gameItemsSizes[103001001] = ((128, 128), (256, 256), (256, 256))
     return gameItemsSizes
 
 gameItemsSizes = setUpSize()
