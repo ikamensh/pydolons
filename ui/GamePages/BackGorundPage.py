429b3e856729d1cc7254903d4897d02b6d52e834
reef425 <reef45@gmail.com>
2018-12-18 19:31:40 +0500

    add background module

---
+++ ui/GamePages/BackGorundPage.py
@@ -0,0 +1,23 @@
+from PySide2 import QtWidgets
+
+from ui.GamePages import AbstractPage
+
+
+class BackGorundPage(AbstractPage):
+    """docstring for StartPage."""
+    def __init__(self, gamePages):
+        super().__init__(gamePages)
+        self.state = True
+        self.setZValue(-10)
+        self.setUpWidgets()
+        self.gamePages.gameRoot.view.wheel_change.connect(self.updatePos)
+
+    def setUpWidgets(self):
+        self.background = QtWidgets.QGraphicsPixmapItem(self.gamePages.gameRoot.cfg.getPicFile('dungeon.jpg'))
+        self.resizeBackground(self.background)
+        self.addToGroup(self.background)
+
+    def resized(self):
+        super().resized()
+        self.resizeBackground(self.background)
+        pass
