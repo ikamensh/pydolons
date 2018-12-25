e8759b494f02fd1928b12fc7ffe3743ee467d722
reef425 <reef45@gmail.com>
2018-12-25 23:44:09 +0500

    update size

--- ui/GameSizeConfig.py
+++ ui/GameSizeConfig.py
@@ -5,10 +5,10 @@ def setUpSize():
     gameItemsSizes[101002001] = ((32, 32), (64, 64), (96, 96))
     gameItemsSizes[101002002] = ((128, 128), (256, 256), (384, 384))
     # MasteriesPage
-    gameItemsSizes[101003001] = ((32, 32), (64, 64), (128, 128))
+    gameItemsSizes[101003001] = ((32, 32), (64, 64), (96, 96))
     gameItemsSizes[101003002] = ((128, 128), (256, 256), (384, 384))
     # PerksPage
-    gameItemsSizes[101004001] = ((32, 32), (64, 64), (128, 128))
+    gameItemsSizes[101004001] = ((32, 32), (64, 64), (96, 96))
     gameItemsSizes[101004002] = ((128, 128), (256, 256), (384, 384))
     # World size
     # Floor
