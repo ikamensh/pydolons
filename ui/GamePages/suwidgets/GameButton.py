ec69511b33e3b75e7c1b06391f849c3d38bd7998
Reef <reef425@gmail.com>
2019-01-03 19:01:33 +0500

    add reimplemntation QPushButton module GameButton

---
+++ ui/GamePages/suwidgets/GameButton.py
@@ -0,0 +1,18 @@
+from PySide2 import QtCore, QtWidgets
+
+
+class GameButton(QtWidgets.QPushButton):
+    hovered = QtCore.Signal(QtCore.QObject)
+    hover_out = QtCore.Signal(QtCore.QObject)
+    def __init__(self, *args, parent = None):
+        super(GameButton, self).__init__(*args, parent)
+        self.setAttribute(QtCore.Qt.WA_Hover)
+        self.installEventFilter(self)
+
+    def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent):
+        if event.type() == QtCore.QEvent.HoverEnter:
+            watched.hovered.emit(watched)
+            pass
+        elif event.type() == QtCore.QEvent.HoverLeave:
+            watched.hover_out.emit(watched)
+        return QtWidgets.QWidget.eventFilter(self, watched, event)
