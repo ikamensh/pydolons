09e511372432c4f3a108e841fe112aaeee7dc042
Reef <reef425@gmail.com>
2019-01-11 11:01:55 +0500

    refactoring

--- ui/GamePages/suwidgets/items/on_unit/EquipmentWidget.py
+++ ui/GamePages/suwidgets/items/on_unit/EquipmentWidget.py
@@ -18,30 +18,30 @@ class EquipmentWidget(QtWidgets.QWidget):
         layout = QtWidgets.QGridLayout()
         print(self.the_hero.equipment.all_slots)
         for game_slot in self.the_hero.equipment.all_slots:
-            if game_slot.name.lower() == str(EquipmentSlotUids.HEAD):
+            if game_slot.name == EquipmentSlotUids.HEAD.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 0, 2)
-            elif game_slot.name.lower() == str(EquipmentSlotUids.BODY):
+            elif game_slot.name == EquipmentSlotUids.BODY.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name, h = 128)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 1, 2)
-            elif game_slot.name.lower() == str(EquipmentSlotUids.RING_1):
+            elif game_slot.name == EquipmentSlotUids.RING_1.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 1, 0)
-            elif game_slot.name.lower() == str(EquipmentSlotUids.RING_2):
+            elif game_slot.name == EquipmentSlotUids.RING_2.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 1, 4)
-            elif game_slot.name.lower() == str(EquipmentSlotUids.HANDS):
+            elif game_slot.name == EquipmentSlotUids.HANDS.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name, h = 128)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 1, 1)
                 slot = self.getSlotWiget(game_slot, game_slot.name, h = 128)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 1, 3)
-            elif game_slot.name.lower() == str(EquipmentSlotUids.FEET):
+            elif game_slot.name == EquipmentSlotUids.FEET.name:
                 slot = self.getSlotWiget(game_slot, game_slot.name, h = 128)
                 self.setPicSlot(game_slot, slot)
                 layout.addWidget(slot, 3, 2)
@@ -56,7 +56,6 @@ class EquipmentWidget(QtWidgets.QWidget):
         slot.setProperty('item_type', game_slot.item_type)
         slot.setProperty('name', game_slot.name)
         slot.setProperty('slot', game_slot)
-        slot.setProperty('info', self.info)
         slot.setStyleSheet(self.slot_style)
         slot.setFixedSize(w, h)
         self.slots[game_slot.name] = slot
@@ -69,16 +68,13 @@ class EquipmentWidget(QtWidgets.QWidget):
             pixmap = self.cfg.getPicFile(game_slot.content.icon,101005001)
         slot.setPixmap(pixmap)
 
-    def info(self, slot):
-        if slot.property('slot').content is None:
-            return 'Slot empty'
-        else:
-            return str(slot.property('slot').content)
-
     def toolTipShow(self, widget):
         pos = self.parent().parent().mapToParent(widget.pos())
         x, y = pos.x(), pos.y()
         self.page.gamePages.toolTip.setPos(x, y)
-        self.page.gamePages.toolTip.setText(self.info(widget))
+        if widget.property('slot').content is None:
+            self.page.gamePages.toolTip.setText('Slot empty')
+        else:
+            self.page.gamePages.toolTip.setDict(widget.property('slot').tooltip_info)
         self.page.gamePages.toolTip.show()
         pass
