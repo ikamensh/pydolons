403fe20c236a57c14e0d38f06d182490758914a0
Reef <reef425@gmail.com>
2019-01-11 14:48:22 +0500

    refactoring

--- ui/GamePages/suwidgets/items/on_unit/InventoryWidget.py
+++ ui/GamePages/suwidgets/items/on_unit/InventoryWidget.py
@@ -1,28 +1,23 @@
 from PySide2 import QtWidgets
 from ui.GamePages.suwidgets.items.SlotWidget import SlotWidget
 from ui.GamePages.suwidgets.items.on_unit.SlotGroupWidget import SlotGroupWidget
+from ui.GamePages.suwidgets.items.GropsTypes import GropusTypes
 
 
 class InventoryWidget(SlotGroupWidget):
 
     def __init__(self, page, parent ):
         super(InventoryWidget, self).__init__(page=page, parent=parent)
+        self.type = GropusTypes.INVENTORY
         self.setUpWidgets()
 
     def setUpWidgets(self):
         columns = 5
         row = 0
         layout = QtWidgets.QGridLayout(self)
-
         j = 0
         for i, game_slot in enumerate(self.the_hero.inventory):
-            label = SlotWidget()
-            label.setText('slot_' + str(i + 1))
-            label.hovered.connect(self.toolTipShow)
-            label.hover_out.connect(self.page.toolTipHide)
-            label.slot_changed.connect(self.page.slot_change)
-            label.setFixedSize(64, 64)
-            label.setProperty('slot', game_slot)
+            label = self.getSlotWiget(game_slot, name = 'slot_' + str(i + 1))
             self.setPicSlot(game_slot, label)
             layout.addWidget(label, row, j)
             j += 1
