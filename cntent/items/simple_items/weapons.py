b4ac1890551bddebf3989d940aace9e4a231ee01
Ilya <ikamenshchikov@gmail.com>
2019-01-11 09:03:09 +0100

    доработка методов Slot & Equipment - сообщения, проверки

--- cntent/items/simple_items/weapons.py
+++
@@ -1,3 +0,0 @@
-from game_objects.items import WeaponBlueprint, WeaponTypes, MaterialTypes
-from game_objects.items import WearableItem, ItemTypes
-axe = WearableItem('stone_axe', ItemTypes.WEAPON, material = MaterialTypes.METAL)
