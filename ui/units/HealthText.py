94d6b3197f9f67d601ba94315af47b9d6124f503
Reef <reef425@gmail.com>
2019-02-12 23:36:06 +0500

    refactoring, add def setOffset

--- ui/units/HealthText.py
+++ ui/units/HealthText.py
@@ -1,31 +1,38 @@
-from PySide2 import QtGui, QtWidgets, QtCore
+from PySide2.QtCore import QTimer, Qt
+from PySide2.QtGui import QFont
+from PySide2.QtWidgets import QGraphicsTextItem
 
-class HealthText(QtWidgets.QGraphicsTextItem):
+
+class HealthText(QGraphicsTextItem):
     def __init__(self):
         super(HealthText, self).__init__()
         self.w = 128
         self.h = 128
         # self.setOpacity(1.0)
-        self.setFont(QtGui.QFont("Times", 24, 10, False))
-        self.timer = QtCore.QTimer(self)
+        self.setFont(QFont("Times", 36, 10, False))
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.timerSlot)
         self.setVisible(False)
 
     def setText(self, value):
         self.setOpacity(1.0)
         if value < 0:
-            self.setDefaultTextColor(QtCore.Qt.red)
+            self.setDefaultTextColor(Qt.red)
         else:
-            self.setDefaultTextColor(QtCore.Qt.green)
+            self.setDefaultTextColor(Qt.green)
         self.setPlainText(str(value))
         self.setVisible(True)
         self.timer.start(500)
 
     def setUnitPos(self, pos):
-        self.setPos(pos.x() + 32, pos.y() - 32)
+        self.setPos(pos.x() + 32, pos.y()-64)
 
     def timerSlot(self):
         self.setOpacity(self.opacity() - 0.1)
         if self.opacity() < 0.3:
             self.setVisible(False)
             self.timer.stop()
+
+    def setOffset(self, x, y):
+        # self.setPos(x + 32, y - 32)
+        pass
