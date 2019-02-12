7f42088de074b33a7f1c797adc18cf111a2083c0
Reef <reef425@gmail.com>
2019-02-12 23:49:43 +0500

    refactoring

--- ui/GamePages/suwidgets/NotifyText.py
+++ ui/GamePages/suwidgets/NotifyText.py
@@ -7,8 +7,10 @@ class NotifyText(QtWidgets.QGraphicsTextItem):
         self.gameRoot = None
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.timerSlot)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIgnoresTransformations, True)
         self.setVisible(False)
         self.setZValue(20)
+        self.widget_pos = (0, 0)
 
     def showText(self, text):
         self.setOpacity(1.0)
@@ -17,7 +19,8 @@ class NotifyText(QtWidgets.QGraphicsTextItem):
         y = self.gameRoot.cfg.dev_size[1] - self.boundingRect().height()
         x = x / 2
         y = y / 2
-        self.setPos(x, y)
+        self.widget_pos = x, y
+        self.setPos(self.gameRoot.view.mapToScene(x, y))
         self.setVisible(True)
         self.timer.start(500)
 
@@ -26,3 +29,7 @@ class NotifyText(QtWidgets.QGraphicsTextItem):
         if self.opacity() < 0.3:
             self.setVisible(False)
             self.timer.stop()
+
+    def resized(self):
+        self.setPos(self.gameRoot.view.mapToScene(self.widget_pos[0], self.widget_pos[1]))
+        pass
