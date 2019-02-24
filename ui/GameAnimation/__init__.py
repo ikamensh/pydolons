21e050cbdba0ceeeb4cd77aa3d5019f86ebb3d66
Reef <reef425@gmail.com>
2019-02-24 19:30:51 +0500

    refactoring add

--- ui/GameAnimation/__init__.py
+++ ui/GameAnimation/__init__.py
@@ -1,3 +1,5 @@
 from ui.GameAnimation.Animations import Animations
 from ui.GameAnimation.SmoothAnimation import SmoothAnimation
 from ui.GameAnimation.objects.Direction import Direction
+from ui.GameAnimation.GameVariantAnimation import GameVariantAnimation
+
