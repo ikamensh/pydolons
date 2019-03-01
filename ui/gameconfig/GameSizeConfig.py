8446db598ab24e44cf14ee61b48e66857c8dbfdd
Reef <reef425@gmail.com>
2019-03-01 23:48:07 +0500

    refactoring

--- ui/gameconfig/GameSizeConfig.py
+++ ui/gameconfig/GameSizeConfig.py
@@ -3,7 +3,7 @@ def setUpSize():
     gameItemsSizes = {}
     # CharacterPage sizes
     gameItemsSizes[101002001] = ((32, 32), (64, 64), (96, 96))
-    gameItemsSizes[101002002] = ((128, 128), (256, 256), (384, 384))
+    gameItemsSizes[101002002] = ((128, 128), (152, 152), (256, 256))
     # MasteriesPage
     gameItemsSizes[101003001] = ((32, 32), (64, 64), (96, 96))
     gameItemsSizes[101003002] = ((128, 128), (256, 256), (384, 384))
@@ -22,3 +22,4 @@ def setUpSize():
     return gameItemsSizes
 
 gameItemsSizes = setUpSize()
+
