7ffb58754e96704f33dd5cc946d50d1ca67349fd
Reef <reef425@gmail.com>
2019-03-26 17:21:40 +0500

    change source data for test

--- tests/ui/GamePages/xml/test_xml_engine.py
+++ tests/ui/GamePages/xml/test_xml_engine.py
@@ -50,8 +50,50 @@ a_1_b_1
 
 def test_read_tree():
     from xml.etree import ElementTree as ET
+    data_as_string = """<?xml version="1.0" encoding="UTF-8" ?>
+<page>
+    <item name = 'a_1'>
+        <item name="a_1_b_1"></item>
+        <item name="a_1_b_2">
+            <item name="a_1_b_2_c_1"></item>
+            <item name="a_1_b_2_c_2"></item>
+            <item name="a_1_b_2_c_3"></item>
+        </item>
+        <item name="a_1_b_3">
+            <item name="a_1_b_3_c_1"></item>
+        </item>
+    </item>
+    <item name = 'a_2'>
+        <item name="a_2_b_1"></item>
+        <item name="a_2_b_2">
+            <item name="a_2_b_2_c_1"></item>
+            <item name="a_2_b_2_c_2"></item>
+            <item name="a_2_b_2_c_3"></item>
+        </item>
+        <item name="a_2_b_3"></item>
+        <item name="a_2_b_4">
+            <item name="a_2_b_4_c_1"></item>
+            <item name="a_2_b_4_c_2"></item>
+        </item>
+        <item name="a_2_b_5"></item>
+    </item>
+    <item name = 'a_3'>
+        <item name="a_3_b_1"></item>
+    </item>
+    <item name = 'a_4'>
+        <item name="a_4_b_1">
+            <item name="a_4_b_1_c_1"></item>
+            <item name="a_4_b_1_c_2"></item>
+        </item>
+        <item name="a_4_b_2">
+            <item name="a_4_b_2_c_1"></item>
+        </item>
+    </item>
+</page>
+    """
     xml_path = 'test_page2.xml'
-    tree = ET.parse(xml_path)
-    xml_page: ET.Element = tree.getroot()
+    #tree = ET.parse(xml_path)
+    # xml_page: ET.Element = tree.getroot()
+    xml_page = ET.fromstring(data_as_string)
     res = read_tree(xml_page)
     assert res == OUTPUT
